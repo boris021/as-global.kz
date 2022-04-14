f00e10ec05201368c21877f85a874dca53204921
Boris <boris021@mail.ru>
2022-04-14 13:49:44 +0500

    fix

--- src/js/files/functions.js
+++ src/js/files/functions.js
@@ -51,6 +51,7 @@ if (isMobile.any()) {
 	document.body.classList.add('_touch');
 
 	let menuArrows = document.querySelectorAll('.menu__arrow');
+
 	if (menuArrows.length > 0) {
 		for (let index = 0; index < menuArrows.length; index++) {
 			const menuArrow = menuArrows[index];
